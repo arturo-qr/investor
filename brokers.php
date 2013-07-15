d37974d2adeb1dee082c3072d9c6ffca907cf03d
Rocandio <juanrocandio@hotmail.com>
2013-07-15 16:53:35 -0700

    Brokers icons and some font styles

--- brokers.php
+++ brokers.php
@@ -8,20 +8,20 @@
 				<div class="inner">
 					<div class="style1">Brokers, start trading!</div>
 					</br>
-					<span class="style2">Select broker site of your choice</span>
-					</br>
-					<ul  class="simple">
-						<li><a href="http://www.optionsxpress.com/" target="_blank">optionsXpress</a></li>
-						<li><a href="http://www.optionshouse.com/" target="_blank">OptionsHouse</a></li>
-						<li><a href="https://www.tradeking.com/" target="_blank">TradeKing</a></li>
-						<li><a href="https://www.scottrade.com/" target="_blank">Scottrade</a></li>
-						<li><a href="https://www.fidelity.com/" target="_blank">Fidelity</a></li>
-						<li><a href="https://us.etrade.com/home" target="_blank">E*Trade Financial</a></li>
-						<li><a href="https://www.schwab.com/" target="_blank">Charles Schwab</a></li>
-						<li><a href="https://www.tdameritrade.com/home.page" target="_blank">TD Ameritrade</a></li>
-						<li><a href="http://www.sharebuilder.com/" target="_blank">ShareBuilder</a></li>
-						<li><a href="https://www.firstrade.com/content/en-us/welcome" target="_blank">Firstrade</a></li>
-					</ul>
+					<span class="style2">Select broker site of your choice:</span>
+					</br></br>
+					<div class="simple" style="background-color:#FFF;padding:20px;">
+						<a href="http://www.optionsxpress.com/" target="_blank"><img style="padding:30px;" src="images/optionsXpress.png" width="200" height="50"></a>
+						<a href="http://www.optionshouse.com/" target="_blank"><img style="padding:30px;" src="images/optionshouse.png" width="200" height="50"></a>
+						<a href="https://www.tradeking.com/" target="_blank"><img style="padding:30px;" src="images/tradeking_logo.png" width="200" height="50"></a>
+						<a href="https://www.scottrade.com/" target="_blank"><img style="padding:30px;" src="images/scottrade.png" width="200" height="50"></a>
+						<a href="https://www.fidelity.com/" target="_blank"><img style="padding:30px;" src="images/fidelity-logo-nav.gif" width="200" height="50"></a>
+						<a href="https://us.etrade.com/home" target="_blank"><img style="padding:30px;" src="images/500px-ETrade.svg.png" width="200" height="50"></a>
+						<a href="https://www.schwab.com/" target="_blank"><img style="padding:30px;" src="images/21_charles-schwab.jpg" width="200" height="50"></a>
+						<a href="https://www.tdameritrade.com/home.page" target="_blank"><img style="padding:30px;" src="images/utility-logo.png" width="200" height="50"></a>
+						<a href="http://www.sharebuilder.com/" target="_blank"><img style="padding:30px;" src="images/sharebuilder-204x70.jpg" width="200" height="50"></a>
+						<a href="https://www.firstrade.com/content/en-us/welcome" target="_blank"><img style="padding:30px;" src="images/firstrade logo.gif" width="200" height="50"></a>
+					</div>
 				</div>
 			</div>
 		</div>
